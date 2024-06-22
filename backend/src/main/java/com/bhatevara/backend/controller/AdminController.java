cc4c10ec2073c189596c091b7c5f2f1654eeef99
yaswanth <yaswanth169choudary@gmail.com>
2024-06-22 17:56:24 +0530

    Avoid this

--- backend/src/main/java/com/bhatevara/backend/controller/AdminController.java
+++ backend/src/main/java/com/bhatevara/backend/controller/AdminController.java
@@ -1,12 +1,37 @@
 package com.bhatevara.backend.controller;
 
 
+import com.bhatevara.backend.entity.User;
+import com.bhatevara.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/apis/admin")
 public class AdminController {
 
-
+//    @Autowired
+//    UserService userService;
+//    @GetMapping("/users")
+//    public ResponseEntity<List<User>> getAllUsers() {
+//        try {
+//            List<User> users = userService.finadAll();
+//
+//            if (users.isEmpty()) {
+//                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//            }
+//
+//            return new ResponseEntity<>(users, HttpStatus.OK);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
 }
