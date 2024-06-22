1a05aa9cebdd41322f999c8cf718e7fc96336820
yaswanth <yaswanth169choudary@gmail.com>
2024-06-22 14:55:23 +0530

    Renamed Repo Name , Logic added for saving the object in database..

--- backend/src/main/java/com/bhatevara/backend/repository/ApplicationFormRepo.java
+++ backend/src/main/java/com/bhatevara/backend/repository/ApplicationFormRepo.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface applicationformrepo extends JpaRepository<ApplicationForm,String> {
+public interface ApplicationFormRepo extends JpaRepository<ApplicationForm,String> {
     //if needed lets implement some methods...
-
 }
--- backend/src/main/java/com/bhatevara/backend/service/ApplicationFormService.java
+++ backend/src/main/java/com/bhatevara/backend/service/ApplicationFormService.java
@@ -1,6 +1,9 @@
 package com.bhatevara.backend.service;
 
 
+import com.bhatevara.backend.entity.ApplicationForm;
+
 public interface ApplicationFormService {
+    public void AddForm(ApplicationForm r);
 
 }
