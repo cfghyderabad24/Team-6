2debec188e26a51cbcf5bba9b1bfc04bd2d4474b
G-Bhavagna <gaddebhavagna1@gmail.com>
2024-06-23 00:30:06 +0530

    Added the first level validation and submitted and fetched data through application api

--- frontend/cfg/src/services/ApplicationService.js
+++ frontend/cfg/src/services/ApplicationService.js
@@ -15,3 +15,26 @@ export const submitApplication = async (formData) => {
     throw error;
   }
 };
+
+export const fetchApplications = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/allapps`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching applications:', error);
+    throw error;
+  }
+};
+
+
+export const approveApplication = async (id, remark) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/approve/${id}`, null, {
+      params: { remark },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error approving application:', error);
+    throw error;
+  }
+};
