5333df165361704e8420ae587c2277245d49fc7f
G-Bhavagna <gaddebhavagna1@gmail.com>
2024-06-22 19:52:46 +0530

    changes

--- backend/src/main/java/com/bhatevara/backend/repository/NgoEmployeeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.bhatevara.backend.repository;
-
-import com.bhatevara.backend.entity.NgoEmployee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NgoEmployeeRepo extends JpaRepository<NgoEmployee, Long> {
-}
--- backend/src/main/java/com/bhatevara/backend/service/ApplicationFormService.java
+++
@@ -1,9 +0,0 @@
-package com.bhatevara.backend.service;
-
-
-import com.bhatevara.backend.entity.ApplicationForm;
-
-public interface ApplicationFormService {
-    public ApplicationForm AddForm(ApplicationForm r);
-
-}
