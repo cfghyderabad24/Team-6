927605b2d0cd1c6039ace7793f1e975b255d252c
yaswanth <yaswanth169choudary@gmail.com>
2024-06-22 15:00:29 +0530

    Common api added

--- backend/src/main/java/com/bhatevara/backend/controller/ApplicationController.java
+++ backend/src/main/java/com/bhatevara/backend/controller/ApplicationController.java
@@ -1,17 +1,17 @@
 package com.bhatevara.backend.controller;
 
 import com.bhatevara.backend.entity.ApplicationForm;
-import com.bhatevara.backend.repository.applicationformrepo;
+import com.bhatevara.backend.repository.ApplicationFormRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/apis/bhatevara")
 public class ApplicationController {
-
-
 
     @PostMapping("/submitForm")
     public ResponseEntity<ApplicationForm> submitForm(@RequestBody ApplicationForm form) {
