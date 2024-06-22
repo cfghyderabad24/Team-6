23e49451e0e66e509f1cd66a2d0176d8f0236b12
Bargav12 <144159609+Bargav12@users.noreply.github.com>
2024-06-22 17:04:07 -0500

    Add files via upload

--- Sidebar.jsx
+++ Sidebar.jsx
@@ -1,43 +1,36 @@
-import React from 'react'
-import 
-{BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, 
-  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
- from 'react-icons/bs'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill } from 'react-icons/bs';
+import './Sidebar.css';
 
-function Sidebar({openSidebarToggle, OpenSidebar}) {
+function Sidebar({ openSidebarToggle, OpenSidebar }) {
   return (
-    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
-        <div className='sidebar-title'>
-            <div className='sidebar-brand'>
-                <div  className='icon_header'> St. Bhatevara</div>
-            </div>
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive" : ""}>
+      <div className='sidebar-title'>
+        <div className='sidebar-brand'>
+          <div className='icon_header'> St. Bhatevara</div>
         </div>
+      </div>
 
-        <ul className='sidebar-list'>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsGrid1X2Fill className='icon'/> Student Registration
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsFillArchiveFill className='icon'/> Partner NGO
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsFillGrid3X3GapFill className='icon'/> Volunteer 
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsPeopleFill className='icon'/> Alumni
-                </a>
-            </li>
-            
-        </ul>
+      <ul className='sidebar-list'>
+        <li className='sidebar-list-item'>
+          <Link to="/partner-ngo">
+            <BsFillArchiveFill className='icon' /> Partner NGO
+          </Link>
+        </li>
+        <li className='sidebar-list-item'>
+          <Link to="/volunteer">
+            <BsFillGrid3X3GapFill className='icon' /> Volunteer
+          </Link>
+        </li>
+        <li className='sidebar-list-item'>
+          <Link to="/students">
+            <BsPeopleFill className='icon' /> Students
+          </Link>
+        </li>
+      </ul>
     </aside>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
