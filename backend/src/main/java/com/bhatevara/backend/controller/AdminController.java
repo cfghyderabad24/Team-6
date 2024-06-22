b876644c0381863cf44ce497c44146589008c56d
yaswanth <yaswanth169choudary@gmail.com>
2024-06-23 00:01:18 +0530

    Commiting -> AdminController , ApplicationController, PartnerController , VolunteerController....

--- backend/src/main/java/com/bhatevara/backend/controller/AdminController.java
+++ backend/src/main/java/com/bhatevara/backend/controller/AdminController.java
@@ -30,45 +30,136 @@ public class AdminController {
         return "applicationSuccess";
     }
 
-    @GetMapping("/applications/pending")
-    public String getPendingApplications(Model model) {
-        List<ApplicationForm> pendingApplications = applicationFormService.getPendingApplications();
-        model.addAttribute("applications", pendingApplications);
-        return "pendingApplications";
-    }
+//    @GetMapping("/applications/pending")
+//    public String getPendingApplications(Model model) {
+//        List<ApplicationForm> pendingApplications = applicationFormService.getPendingApplications();
+//        model.addAttribute("applications", pendingApplications);
+//        return "pendingApplications";
+//    }
 
-    @PostMapping("/applications/{id}/approve")
-    public String approveApplication(@PathVariable Long id, @RequestParam("remark") String remark, Model model) {
-        applicationFormService.updatePngoStatus(id, true, remark);
-        return "redirect:/admin/applications/pending";
-    }
+//    @PostMapping("/applications/{id}/approve")
+//    public String approveApplication(@PathVariable Long id, @RequestParam("remark") String remark, Model model) {
+//        applicationFormService.updatePngoStatus(id, true, remark);
+//        return "redirect:/admin/applications/pending";
+//    }
 
-    @PostMapping("/applications/{id}/reject")
-    public String rejectApplication(@PathVariable Long id, @RequestParam("remark") String remark, Model model) {
-        applicationFormService.updatePngoStatus(id, false, remark);
-        return "redirect:/admin/applications/pending";
-    }
+//    @PostMapping("/applications/{id}/reject")
+//    public String rejectApplication(@PathVariable Long id, @RequestParam("remark") String remark, Model model) {
+//        applicationFormService.updatePngoStatus(id, false, remark);
+//        return "redirect:/admin/applications/pending";
+//    }
 
 
-    @GetMapping("/applications/volunteer")
-    public String getApplicationsReadyForVolunteer(Model model) {
-        List<ApplicationForm> readyApplications = applicationFormService.getApplicationsReadyForVolunteer();
-        model.addAttribute("applications", readyApplications);
-        return "volunteerPendingApplications";
-    }
+//    @GetMapping("/applications/volunteer")
+//    public String getApplicationsReadyForVolunteer(Model model) {
+//        List<ApplicationForm> readyApplications = applicationFormService.getApplicationsReadyForVolunteer();
+//        model.addAttribute("applications", readyApplications);
+//        return "volunteerPendingApplications";
+//    }
 
-    @PostMapping("/applications/{id}/verify")
-    public String verifyApplication(@PathVariable Long id, @RequestParam("remark") String remark, @RequestParam("status") boolean status, Model model) {
-        applicationFormService.updateVolunteerStatus(id, status, remark);
-        return "redirect:/admin/applications/volunteer";
-    }
+//    @PostMapping("/applications/{id}/verify")
+//    public String verifyApplication(@PathVariable Long id, @RequestParam("remark") String remark, @RequestParam("status") boolean status, Model model) {
+//        applicationFormService.updateVolunteerStatus(id, status, remark);
+//        return "redirect:/admin/applications/volunteer";
+//    }
 
     //final
+//    @GetMapping("/applications/final")
+//    public String getApplicationsForFinalDecision(Model model) {
+//        List<ApplicationForm> finalApplications = applicationFormService.getApplicationsForFinalDecision();
+//        model.addAttribute("applications", finalApplications);
+//        return "finalDecisionApplications";
+//    }
+
+//    @PostMapping("/applications/{id}/finalize")
+//    public String finalizeApplication(
+//            @PathVariable Long id,
+//            @RequestParam("decision") String decision,
+//            @RequestParam("remark") String remark,
+//            @RequestParam("amount") double amount,
+//            @RequestParam("chequeNumber") String chequeNumber,
+//            @RequestParam("chequeDate") String chequeDate,
+//            @RequestParam("chequePayee") String chequePayee,
+//            Model model) {
+//        applicationFormService.updateFinalDecision(id, decision, remark, amount, chequeNumber, chequeDate, chequePayee);
+//        return "redirect:/admin/applications/final";
+//    }
+
+
+
+
+//    @GetMapping("/applications/{id}/document/{type}")
+//    public ResponseEntity<byte[]> viewDocument(@PathVariable Long id, @PathVariable String type) {
+//        Optional<ApplicationForm> optionalApplicationForm = applicationFormService.getApplicationFormById(id);
+//        if (optionalApplicationForm.isPresent()) {
+//            ApplicationForm applicationForm = optionalApplicationForm.get();
+//            byte[] document = null;
+//            String filename = "";
+//
+//            switch (type) {
+//                case "tenthCertificate":
+//                    document = applicationForm.getTenthCertificate();
+//                    filename = "10thCertificate.pdf";
+//                    break;
+//                case "twelfthCertificate":
+//                    document = applicationForm.getTwelfthCertificate();
+//                    filename = "12thCertificate.pdf";
+//                    break;
+//                case "bankStatements":
+//                    document = applicationForm.getBankStatements();
+//                    filename = "BankStatements.pdf";
+//                    break;
+//                case "aadhaarCard":
+//                    document = applicationForm.getAadhaarCard();
+//                    filename = "AadhaarCard.pdf";
+//                    break;
+//                case "panCard":
+//                    document = applicationForm.getPanCard();
+//                    filename = "PanCard.pdf";
+//                    break;
+//                case "incomeProof":
+//                    document = applicationForm.getIncomeProof();
+//                    filename = "IncomeProof.pdf";
+//                    break;
+//                case "currentYearFeeReceipt":
+//                    document = applicationForm.getCurrentYearFeeReceipt();
+//                    filename = "CurrentYearFeeReceipt.pdf";
+//                    break;
+//                case "feeStructure":
+//                    document = applicationForm.getFeeStructure();
+//                    filename = "FeeStructure.pdf";
+//                    break;
+//                case "lastYearFeeReceipts":
+//                    document = applicationForm.getLastYearFeeReceipts();
+//                    filename = "LastYearFeeReceipts.pdf";
+//                    break;
+//                case "essay":
+//                    document = applicationForm.getEssay();
+//                    filename = "Essay.pdf";
+//                    break;
+//                case "admissionConfirmationLetter":
+//                    document = applicationForm.getAdmissionConfirmationLetter();
+//                    filename = "AdmissionConfirmationLetter.pdf";
+//                    break;
+//                default:
+//                    return ResponseEntity.notFound().build();
+//            }
+//
+//            if (document != null) {
+//                return ResponseEntity.ok()
+//                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + filename + "\"")
+//                        .contentType(MediaType.APPLICATION_PDF)
+//                        .body(document);
+//            }
+//        }
+//        return ResponseEntity.notFound().build();
+//    }
+
+
     @GetMapping("/applications/final")
-    public String getApplicationsForFinalDecision(Model model) {
+    public List<ApplicationForm> getApplicationsForFinalDecision() {
         List<ApplicationForm> finalApplications = applicationFormService.getApplicationsForFinalDecision();
-        model.addAttribute("applications", finalApplications);
-        return "finalDecisionApplications";
+        return finalApplications;
     }
 
     @PostMapping("/applications/{id}/finalize")
@@ -79,80 +170,9 @@ public class AdminController {
             @RequestParam("amount") double amount,
             @RequestParam("chequeNumber") String chequeNumber,
             @RequestParam("chequeDate") String chequeDate,
-            @RequestParam("chequePayee") String chequePayee,
-            Model model) {
+            @RequestParam("chequePayee") String chequePayee) {
         applicationFormService.updateFinalDecision(id, decision, remark, amount, chequeNumber, chequeDate, chequePayee);
-        return "redirect:/admin/applications/final";
-    }
-
-
-
-
-    @GetMapping("/applications/{id}/document/{type}")
-    public ResponseEntity<byte[]> viewDocument(@PathVariable Long id, @PathVariable String type) {
-        Optional<ApplicationForm> optionalApplicationForm = applicationFormService.getApplicationFormById(id);
-        if (optionalApplicationForm.isPresent()) {
-            ApplicationForm applicationForm = optionalApplicationForm.get();
-            byte[] document = null;
-            String filename = "";
-
-            switch (type) {
-                case "tenthCertificate":
-                    document = applicationForm.getTenthCertificate();
-                    filename = "10thCertificate.pdf";
-                    break;
-                case "twelfthCertificate":
-                    document = applicationForm.getTwelfthCertificate();
-                    filename = "12thCertificate.pdf";
-                    break;
-                case "bankStatements":
-                    document = applicationForm.getBankStatements();
-                    filename = "BankStatements.pdf";
-                    break;
-                case "aadhaarCard":
-                    document = applicationForm.getAadhaarCard();
-                    filename = "AadhaarCard.pdf";
-                    break;
-                case "panCard":
-                    document = applicationForm.getPanCard();
-                    filename = "PanCard.pdf";
-                    break;
-                case "incomeProof":
-                    document = applicationForm.getIncomeProof();
-                    filename = "IncomeProof.pdf";
-                    break;
-                case "currentYearFeeReceipt":
-                    document = applicationForm.getCurrentYearFeeReceipt();
-                    filename = "CurrentYearFeeReceipt.pdf";
-                    break;
-                case "feeStructure":
-                    document = applicationForm.getFeeStructure();
-                    filename = "FeeStructure.pdf";
-                    break;
-                case "lastYearFeeReceipts":
-                    document = applicationForm.getLastYearFeeReceipts();
-                    filename = "LastYearFeeReceipts.pdf";
-                    break;
-                case "essay":
-                    document = applicationForm.getEssay();
-                    filename = "Essay.pdf";
-                    break;
-                case "admissionConfirmationLetter":
-                    document = applicationForm.getAdmissionConfirmationLetter();
-                    filename = "AdmissionConfirmationLetter.pdf";
-                    break;
-                default:
-                    return ResponseEntity.notFound().build();
-            }
-
-            if (document != null) {
-                return ResponseEntity.ok()
-                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + filename + "\"")
-                        .contentType(MediaType.APPLICATION_PDF)
-                        .body(document);
-            }
-        }
-        return ResponseEntity.notFound().build();
+        return "Finalized successed...";
     }
 
     @GetMapping("/applications/renewal/volunteer")
--- backend/src/main/java/com/bhatevara/backend/controller/PartnerController.java
+++ backend/src/main/java/com/bhatevara/backend/controller/PartnerController.java
@@ -1,10 +1,39 @@
 package com.bhatevara.backend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.bhatevara.backend.entity.ApplicationForm;
+import com.bhatevara.backend.service.ApplicationFormService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
 public class PartnerController {
+
+    @Autowired
+    private ApplicationFormService applicationFormService;
+
+    @GetMapping("/applications/pending")
+    public List<ApplicationForm> getPendingApplications() {
+        List<ApplicationForm> pendingApplications = applicationFormService.getPendingApplications();
+        return pendingApplications;
+    }
+
+    @PostMapping("/applications/{id}/approve")
+    public String approveApplication(@PathVariable Long id, @RequestParam("remark") String remark) {
+        applicationFormService.updatePngoStatus(id, true, remark);
+        return "Success Approving";
+    }
+
+
+    @PostMapping("/applications/{id}/reject")
+    public String rejectApplication(@PathVariable Long id, @RequestParam("remark") String remark) {
+        applicationFormService.updatePngoStatus(id, false, remark);
+        return "Rejection is Done..";
+    }
+
+
 
 }
