9919176e70df50965770b0698a89a55360b2c8de
G-Bhavagna <gaddebhavagna1@gmail.com>
2024-06-22 13:51:00 +0530

    Create Spring Boot project

---
+++ backend/src/test/java/com/bhatevara/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhatevara.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
